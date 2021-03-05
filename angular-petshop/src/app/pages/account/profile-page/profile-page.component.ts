5035f9041d9e168a683e95d2434f28e7df28faf7
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2021-03-04 22:02:25 -0300

    Creating profile page

---
+++ angular-petshop/src/app/pages/account/profile-page/profile-page.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import { ToastrService } from 'ngx-toastr';
+import { CustomValidator } from 'src/app/validators/custom.validator';
+
+@Component({
+  selector: 'app-profile-page',
+  templateUrl: './profile-page.component.html',
+  styleUrls: ['./profile-page.component.css'],
+})
+export class ProfilePageComponent implements OnInit {
+  public form!: FormGroup;
+  public busy = false;
+
+  constructor(
+    private router: Router,
+    private dataService: DataService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) {
+    this.form = this.formBuilder.group({
+      name: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(80),
+          Validators.required,
+        ]),
+      ],
+      document: [{ value: '', disabled: true }],
+      email: [
+        '',
+        Validators.compose([
+          Validators.minLength(5),
+          Validators.maxLength(120),
+          Validators.required,
+          CustomValidator.EmailValidator,
+        ]),
+      ],
+    });
+  }
+
+  ngOnInit(): void {}
+}
