1775dc9b0984aeb1173a101aed949b83a49b3cb9
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2021-03-04 22:02:41 -0300

    Importing toastr

--- angular-petshop/src/app/app.module.ts
+++ angular-petshop/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ProfilePageComponent } from './pages/account/profile-page/profile-page.component';
+import { DataService } from './services/data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginPageComponent,
-    LoadingComponent
+    LoadingComponent,
+    ProfilePageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DataService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
