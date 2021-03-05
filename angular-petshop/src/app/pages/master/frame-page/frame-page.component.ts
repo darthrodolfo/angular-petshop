9db6ce4a4226eb7b0351101177a1903131f1109c
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2021-03-04 22:17:27 -0300

    Adding master page

---
+++ angular-petshop/src/app/pages/master/frame-page/frame-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frame-page',
+  templateUrl: './frame-page.component.html',
+})
+export class FramePageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
